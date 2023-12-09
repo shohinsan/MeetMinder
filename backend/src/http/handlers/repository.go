dbc3d178f5cfe3bc6afbe5bcf644bfe48d8ab314
Rha02 <romuzus08@gmail.com>
2023-12-09 15:58:53 -0800

    Created Helper Func to Decode JSON Body From Request

--- backend/src/http/handlers/repository.go
+++ backend/src/http/handlers/repository.go
@@ -1,6 +1,12 @@
 package handlers
 
-import "shohinsan/MeetMinder/src/dbrepo"
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+	"shohinsan/MeetMinder/src/dbrepo"
+)
 
 type Repository struct {
 	DB dbrepo.DatabaseRepository
@@ -16,4 +22,23 @@ var Repo *Repository
 
 func NewHandlers(repo *Repository) {
 	Repo = repo
+}
+
+func decodeRequestJSON(w http.ResponseWriter, r *http.Request, dst interface{}) error {
+	defer r.Body.Close()
+
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+
+	err := dec.Decode(dst)
+	if err != nil {
+		return err
+	}
+
+	err = dec.Decode(&struct{}{})
+	if err != io.EOF {
+		return errors.New("body must only have a single JSON value")
+	}
+
+	return nil
 }
--- backend/src/http/handlers/user_handlers.go
+++ backend/src/http/handlers/user_handlers.go
@@ -1,7 +1,23 @@
 package handlers
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 func (app *Repository) Login(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Login page"))
+	// create request body struct
+	body := struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}{}
+
+	defer r.Body.Close()
+
+	// decode request body into struct
+	decodeRequestJSON(w, r, &body)
+
+	log.Println(body)
+
+	w.Write([]byte("Login"))
 }
