049b981fb60605fadb4240a5f93c513e10b8abb3
Rha02 <romuzus08@gmail.com>
2023-12-15 17:17:32 -0800

    Created Meetings Model and DB Queries

--- backend/src/dbrepo/repository.go
+++ backend/src/dbrepo/repository.go
@@ -11,4 +11,19 @@ type DatabaseRepository interface {
 
 	// Get a user by id, returns the user or an error
 	GetUserById(id int64) (*models.User, error)
+
+	// Create a new meeting, returns the new meeting or an error
+	CreateMeeting(meeting *models.Meeting) (*models.Meeting, error)
+
+	// Get a meeting by id, returns the meeting or an error
+	GetMeetingById(id int64) (*models.Meeting, error)
+
+	// Get all meetings for a user, returns a list of meetings or an error
+	GetMeetingsForUser(userId int64) ([]*models.Meeting, error)
+
+	// Update a meeting, returns the updated meeting or an error
+	UpdateMeeting(meeting *models.Meeting) (*models.Meeting, error)
+
+	// Delete a meeting by id, returns an error if something goes wrong
+	DeleteMeeting(id int64) error
 }
--- backend/src/dbrepo/test_repo.go
+++ backend/src/dbrepo/test_repo.go
@@ -33,3 +33,28 @@ func (m *testDBRepo) GetUserById(id int64) (*models.User, error) {
 
 	return &models.User{}, nil
 }
+
+// CreateMeeting implements DatabaseRepository.
+func (*testDBRepo) CreateMeeting(meeting *models.Meeting) (*models.Meeting, error) {
+	panic("unimplemented")
+}
+
+// DeleteMeeting implements DatabaseRepository.
+func (*testDBRepo) DeleteMeeting(id int64) error {
+	panic("unimplemented")
+}
+
+// GetMeetingById implements DatabaseRepository.
+func (*testDBRepo) GetMeetingById(id int64) (*models.Meeting, error) {
+	panic("unimplemented")
+}
+
+// GetMeetingsForUser implements DatabaseRepository.
+func (*testDBRepo) GetMeetingsForUser(userId int64) ([]*models.Meeting, error) {
+	panic("unimplemented")
+}
+
+// UpdateMeeting implements DatabaseRepository.
+func (*testDBRepo) UpdateMeeting(meeting *models.Meeting) (*models.Meeting, error) {
+	panic("unimplemented")
+}
