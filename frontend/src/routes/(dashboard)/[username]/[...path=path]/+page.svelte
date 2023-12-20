b968d6a958b6a12bb997930c414497f18368b8af
Shohin Abdulkhamidov <abd.shohin@gmail.com>
2023-12-19 19:50:43 -0800

    prepare username page for next steps in ui

--- frontend/src/routes/(dashboard)/[username]/[...path=path]/+page.svelte
+++ frontend/src/routes/(dashboard)/[username]/[...path=path]/+page.svelte
@@ -1,6 +1,17 @@
 <script lang="ts">
-    import {page} from '$app/stores';
+    import { enhance } from '$app/forms';
 </script>
 
-
-Hey this is username page
+<div class="flex flex-wrap h-screen p-4 items-center justify-center">
+    <div class="p-4 border bg-white rounded-lg items-center w-[1000px] shadow-lg flex flex-col gap-2 bg-opacity-30 rounded relative">
+        <form method="POST" use:enhance>
+            <div class="mb-96">
+                This is your username page
+            </div>
+        </form>
+        <!-- Powered by text in the bottom-right corner -->
+        <div class="absolute bottom-4 right-4 text-gray-500">
+            Powered by <a href="http://localhost:5173/">meetminder</a>
+        </div>
+    </div>
+</div>
